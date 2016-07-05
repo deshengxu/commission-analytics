ec1a778a7a1cff836ada2266af5530480af32dc4
xudesheng <xudesheng@yahoo.com>
2016-07-05 13:56:12 -0400

    tentative work.

---
+++ ca/folder_validator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# ...
+
+def validate_current_folder(folder):
+    '''
+
+    :param folder: folder to be validated
+    folder should be in pattern like: FY16Q1 FY17Q3 etc.
+    :return: validated FY-year quarter.
+      for example:True FY16 Q1 or False None None
+    '''
+    fy=None
+    cq=None
+    return False, fy, cq
